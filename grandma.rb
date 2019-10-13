f19d29f1cfa4f84e18f44403245b8dacdeb399de
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-13 16:40:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++
@@ -1,7 +0,0 @@
- you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
