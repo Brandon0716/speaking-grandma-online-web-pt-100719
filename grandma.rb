7e43de79a32db35187d9b90e4728ef84195409b9
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-13 17:43:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,9 @@ def speak_to_grandma(phrase)
 
     if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  else 
+     elsif phrase != phrase.upcase
+  else
     "HUH?! SPEAK UP, SONNY!"
-    if phrase == phrase.upcase
+   
     end
 end
