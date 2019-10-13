1b785ce00a6ecadb1bb1f157ca9514168ab7d2d3
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-13 17:29:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,6 @@
 def speak_to_grandma(phrase)
 
-if phrase =="I LOVE YOU GRANDMA!".upcase
-  puts "I LOVE YOU PUMPKIN TOO!".upcase
-elsif phrase == "i miss you"
-  puts "HUH?! SPEAK UP, SONNY!".upcase
-elsif phrase == "HOWS EVERYTHING".upcase
-  puts "NO, NOT SINCE 1938!".upcase
-elsif phrase == "im getting marrried"
-  puts " HUH?! SPEAK UP, SONNY!".upcase
-else phrase == "IM GETTING MARRIED".upcase
-  puts "NO, NOT SINCE 1938!".upcase
+  if phrase == phrase.upcase
+    puts "I LOVE YOU PUMPKIN TOO!"
+  end
 end
