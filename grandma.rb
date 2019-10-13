d6d16f0d73d4949eb25cffc8eca88db804a5afb8
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-13 16:53:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,13 @@
 speak_to_grandma
+
+if speak_to_grandma =="I LOVE YOU GRANDMA!".upcase
+  puts "I LOVE YOU PUMPKIN!".upcase
+elsif speak_to_grandma == "i miss you"
+  puts "HUH?! SPEAK UP, SONNY!".upcase
+elsif speak_to_grandma == "HOWS EVERYTHING".upcase
+  puts "NO, NOT SINCE 1938!".upcase
+elsif speak_to_grandma == "im getting marrried"
+  puts " HUH?! SPEAK UP, SONNY!".upcase
+else speak_to_grandma == "IM GETTING MARRIED".upcase
+  puts "NO, NOT SINCE 1938!".upcase
+end
